121469b672192dfe69634b1294c8d1ddc5846a4d
Vithushan Thayalan <vth@novicell.dk>
2023-02-21 14:10:53 +0100

    wip

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -14,14 +14,16 @@ export const Card: React.FC<CardProps> = ({children, ...props}: PropsWithChildre
     return (
         <div className='card' onClick={props.onClick}>
             <div className='top'>
-            <h5>{props.header}</h5>
-            <p>HP: {props.hp}</p>
+            <h4>{props.header}</h4>
+            <div className='hp'>
+                <p style={{margin:0}}>HP: {props.hp}</p>
             </div>
-            <img width='150px' height="150px" style={{objectFit:'cover'}} src={props.url} alt={props.alt}></img>
+            </div>
+            <img className='pokeimg' width='250px' height="250px" src={props.url} alt={props.alt}></img>
             <div className='move'>
             {props.move?.map((m:string) => {
                 return (
-                    <span>{m}</span>
+                    <span className='move-single'>{m}</span>
                 );
             })}
 
--- src/views/Pokemon/Pokemon.tsx
+++ src/views/Pokemon/Pokemon.tsx
@@ -3,29 +3,65 @@ import './Pokemon.css'
 import { useEffect, useState } from "react";
 import Axios from 'axios'
 import search from '../../assets/search.svg';
+import { Button } from "../../components/button/Button";
+import { Spinner } from "react-bootstrap";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 export default function Pokemon()  {
     const [pokemon, setPokemon] = useState([]);
     const [name, setName] = useState('');
-    const [isLoading, setLoading] = useState(true);
-    const hp = pokemon?.stats?.length ? pokemon?.stats[0]?.base_stat : null;
-    const moves = [pokemon?.moves?.slice(0, 4)]
-    const moveName = moves[0]?.map((m: string) => m.move.name);
+    const [submitted, setSubmitted] = useState('');
+    const [isLoading, setLoading] = useState<boolean>(false);
+    const [error, setError] = useState('');
+    const onePokemon: any = [];
+
+    onePokemon.push({
+        name: pokemon?.name,
+        image: pokemon?.sprites?.front_default,
+        hp: pokemon?.stats?.length ? pokemon?.stats[0]?.base_stat : null,
+        moves:[pokemon?.moves?.slice(0, 4)][0]?.map((m: string) => m.move.name)
+    });
+
     useEffect(() => {
-        Axios.get(`https://pokeapi.co/api/v2/pokemon/${name}`,  {
+        Axios.get(`https://pokeapi.co/api/v2/pokemon/${submitted}`,  {
             method: 'GET',
             headers: {
                 "Content-Type": "application/json",
             },
         }).then((res) => {
-            setLoading(false);
-            setPokemon(res.data);
-            })
-    },[name]);
+            setTimeout(() => {
+                setPokemon(res.data);
+                setLoading(false);
+                setError('')
+            }, 200);
+            }).catch(() => {
+                setError('Pokemon Doesnt exist');        
+            }).finally(() => {
+                setLoading(true);
+            });
+        },[submitted, error]);
 
     const handleChange = (e: any) => {
         setName(e.target.value);
     }
+
+    function handleSubmit(e: any) {
+        setSubmitted(name);
+        if(error.length !== 0) {
+            toast.error(error, {
+              position: "top-right",
+              autoClose: 5000,
+              hideProgressBar: false,
+              closeOnClick: true,
+              pauseOnHover: true,
+              draggable: true,
+              progress: undefined,
+              theme: "light",
+              });
+        }
+      }
    
     return (
        <>
@@ -33,10 +69,30 @@ export default function Pokemon()  {
             <div className="container">
                 <div className="searchField">
                     <img src={search} height='20px' width='20px'></img>
-                    <input  onChange={handleChange} onKeyDown={handleChange} className={'input'} placeholder='Search Pokemon Name'></input>
+                    <input onChange={handleChange} value={name} className={'input'} placeholder='Search Pokemon Name'></input>
+                    <Button onClick={handleSubmit}>{isLoading ? <Spinner animation="border" role="status">
+                        <span className="visually-hidden">Loading...</span>
+                        </Spinner> : 'Search' }
+                    </Button>
                 </div>
-                {name ? <Card header={pokemon?.name} url={pokemon?.sprites?.front_default} hp={hp} move={moveName}></Card> : ''}
+                {submitted ? onePokemon.map((poke: any, index: number) => {
+                    return (
+                        <Card key={index} header={poke.name} url={poke.image} hp={poke.hp} move={poke.moves}></Card>
+                    )
+                }) : ''}
             </div>
+            {error.length !== 0 ? <ToastContainer
+                position="top-right"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="light"
+                /> : ''}
        </>
     ) 
 }
